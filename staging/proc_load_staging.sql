e207e80822a000ef2380af834076d4ecfc896144
taraskvachan <107889485+taraskvachan@users.noreply.github.com>
2025-03-13 20:21:48 +0300

    Update proc_load_staging.sql

--- staging/proc_load_staging.sql
+++ staging/proc_load_staging.sql
@@ -40,148 +40,148 @@ BEGIN
     	special_features, 
     	fulltext
     )
-	SELECT 
-		film_id, 
-		title, 
-		description, 
-		release_year, 
-		language_id, 
-		rental_duration, 
-		rental_rate, 
-		length, 
-		replacement_cost, 
-		rating, 
-		last_update, 
-		special_features, 
-		fulltext
-	FROM 
-		film_src.film;
+    SELECT 
+    	film_id, 
+	title, 
+	description, 
+	release_year, 
+	language_id, 
+	rental_duration, 
+	rental_rate, 
+	length, 
+	replacement_cost, 
+	rating, 
+	last_update, 
+	special_features, 
+	fulltext
+    FROM 
+	film_src.film;
 
-	-- Load staging.inventory
+    -- Load staging.inventory
 	
-	RAISE NOTICE '>> Truncating Table: staging.inventory';
+    RAISE NOTICE '>> Truncating Table: staging.inventory';
     TRUNCATE TABLE staging.inventory;
     
     RAISE NOTICE '>> Inserting Data Into: staging.inventory';
-	INSERT INTO staging.inventory (
-		inventory_id,
-		film_id,
-		store_id,
-		last_update 
-	)
+    INSERT INTO staging.inventory (
+	inventory_id,
+	film_id,
+	store_id,
+	last_update 
+    )
     SELECT 
-		inventory_id,
-		film_id,
-		store_id,
-		last_update 
-	FROM 
-		film_src.inventory;
+	inventory_id,
+	film_id,
+	store_id,
+	last_update 
+    FROM 
+	film_src.inventory;
 
-	-- Load staging.rental
+    -- Load staging.rental
 
-	RAISE NOTICE '>> Truncating Table: staging.rental';
+    RAISE NOTICE '>> Truncating Table: staging.rental';
     TRUNCATE TABLE staging.rental;
     
     RAISE NOTICE '>> Inserting Data Into: staging.rental';
-	INSERT INTO staging.rental (
-		rental_id,
-		rental_date,
-		inventory_id,
-		customer_id,
-		return_date,
-		staff_id,
-		last_update
-	)
+    INSERT INTO staging.rental (
+	rental_id,
+	rental_date,
+	inventory_id,
+	customer_id,
+	return_date,
+	staff_id,
+	last_update
+    )
     SELECT 
-		rental_id,
-		rental_date,
-		inventory_id,
-		customer_id,
-		return_date,
-		staff_id,
-		last_update
-	FROM 
-		film_src.rental;
+	rental_id,
+	rental_date,
+	inventory_id,
+	customer_id,
+	return_date,
+	staff_id,
+	last_update
+    FROM 
+	film_src.rental;
 
-	-- Load staging.payment
+    -- Load staging.payment
 
-	RAISE NOTICE '>> Truncating Table: staging.payment';
+    RAISE NOTICE '>> Truncating Table: staging.payment';
     TRUNCATE TABLE staging.payment;
     
     RAISE NOTICE '>> Inserting Data Into: staging.payment';
-	INSERT INTO staging.payment (	
-		payment_id,
-		customer_id,
-		staff_id,
-		rental_id,
-		amount,
-		payment_date
-	)
-	SELECT
-		payment_id,
-		customer_id,
-		staff_id,
-		rental_id,
-		amount,
-		payment_date
-	FROM
-		film_src.payment;
+    INSERT INTO staging.payment (	
+	payment_id,
+	customer_id,
+	staff_id,
+	rental_id,
+	amount,
+	payment_date
+    )
+    SELECT
+	payment_id,
+	customer_id,
+	staff_id,
+	rental_id,
+	amount,
+	payment_date
+    FROM
+	film_src.payment;
     
     -- Load staging.staff
 
-	RAISE NOTICE '>> Truncating Table: staging.staff';
+    RAISE NOTICE '>> Truncating Table: staging.staff';
     TRUNCATE TABLE staging.staff;
     
     RAISE NOTICE '>> Inserting Data Into: staging.staff';
-	INSERT INTO staging.staff (
-		staff_id,
-		first_name,
-		last_name,
-		store_id
-	)
-	SELECT
-		staff_id,
-		first_name,
-		last_name,
-		store_id
-	FROM
-		film_src.staff;
+    INSERT INTO staging.staff (
+	staff_id,
+	first_name,
+	last_name,
+	store_id
+    )
+    SELECT
+	staff_id,
+	first_name,
+	last_name,
+	store_id
+    FROM
+	film_src.staff;
     
     -- Load staging.address
 
-	RAISE NOTICE '>> Truncating Table: staging.address';
+    RAISE NOTICE '>> Truncating Table: staging.address';
     TRUNCATE TABLE staging.address;
     
     RAISE NOTICE '>> Inserting Data Into: staging.address';
     INSERT INTO staging.address (
     	address_id,
-		address,
-		district,
-		city_id	
+	address,
+	district,
+	city_id	
     )
     SELECT
     	address_id,
-		address,
-		district,
-		city_id	
-	FROM
-		film_src.address;
+	address,
+	district,
+	city_id	
+    FROM
+	film_src.address;
     
     -- Load staging.city
 
-	RAISE NOTICE '>> Truncating Table: staging.city';
+    RAISE NOTICE '>> Truncating Table: staging.city';
     TRUNCATE TABLE staging.city;
     
     RAISE NOTICE '>> Inserting Data Into: staging.city';
     INSERT INTO staging.city (
     	city_id,
-		city
-	)
-	SELECT
-		city_id,
-		city
-	FROM
-		film_src.city;
+	city
+    )
+    SELECT
+	city_id,
+	city
+    FROM
+	film_src.city;
 
     -- Load staging.store
 
@@ -192,12 +192,12 @@ BEGIN
     INSERT INTO staging.store (
     	store_id,
 	address_id 
-	)
-	SELECT
-		store_id,
-		address_id 
-	FROM
-		film_src.store;
+    )
+    SELECT
+	store_id,
+	address_id 
+    FROM
+	film_src.store;
 		
     RAISE NOTICE '==========================================';
     RAISE NOTICE 'Loading Staging Layer is Completed';
