63accbf2455f05362b48e97b699efd00dfef8450
taraskvachan <107889485+taraskvachan@users.noreply.github.com>
2025-03-13 23:30:34 +0300

    Update proc_load_core.sql

--- core/proc_load_core.sql
+++ core/proc_load_core.sql
@@ -10,7 +10,7 @@ Script Purpose:
 ===============================================================================
 */
 
-CREATE OR REPLACE PROCEDURE core.load_core()
+CREATE OR REPLACE PROCEDURE core.load_core(sdate DATE, nm INT)
 LANGUAGE plpgsql
 AS $$
 BEGIN
@@ -77,6 +77,55 @@ BEGIN
     	staging.address a USING(address_id)
     JOIN
     	staging.city c USING(city_id);	
+
+    -- Load core.dim_date
+
+    RAISE NOTICE '>> Truncating Table: core.dim_date';
+    TRUNCATE TABLE core.dim_date;
+    
+    RAISE NOTICE '>> Inserting Data Into: core.dim_date';
+    INSERT INTO core.dim_date 
+    SELECT TO_CHAR(datum, 'yyyymmdd')::INT AS date_dim_id,
+    	datum AS date_actual,
+        EXTRACT(EPOCH FROM datum) AS epoch,
+        TO_CHAR(datum, 'fmDDth') AS day_suffix,
+        TO_CHAR(datum, 'TMDay') AS day_name,
+        EXTRACT(ISODOW FROM datum) AS day_of_week,
+        EXTRACT(DAY FROM datum) AS day_of_month,
+        datum - DATE_TRUNC('quarter', datum)::DATE + 1 AS day_of_quarter,
+        EXTRACT(DOY FROM datum) AS day_of_year,
+        TO_CHAR(datum, 'W')::INT AS week_of_month,
+        EXTRACT(WEEK FROM datum) AS week_of_year,
+        EXTRACT(ISOYEAR FROM datum) || TO_CHAR(datum, '"-W"IW-') || EXTRACT(ISODOW FROM datum) AS week_of_year_iso,
+        EXTRACT(MONTH FROM datum) AS month_actual,
+        TO_CHAR(datum, 'TMMonth') AS month_name,
+        TO_CHAR(datum, 'Mon') AS month_name_abbreviated,
+        EXTRACT(QUARTER FROM datum) AS quarter_actual,
+        CASE
+            WHEN EXTRACT(QUARTER FROM datum) = 1 THEN 'First'
+            WHEN EXTRACT(QUARTER FROM datum) = 2 THEN 'Second'
+            WHEN EXTRACT(QUARTER FROM datum) = 3 THEN 'Third'
+            WHEN EXTRACT(QUARTER FROM datum) = 4 THEN 'Fourth'
+            END AS quarter_name,
+        EXTRACT(YEAR FROM datum) AS year_actual,
+        datum + (1 - EXTRACT(ISODOW FROM datum))::INT AS first_day_of_week,
+        datum + (7 - EXTRACT(ISODOW FROM datum))::INT AS last_day_of_week,
+        datum + (1 - EXTRACT(DAY FROM datum))::INT AS first_day_of_month,
+        (DATE_TRUNC('MONTH', datum) + INTERVAL '1 MONTH - 1 day')::DATE AS last_day_of_month,
+        DATE_TRUNC('quarter', datum)::DATE AS first_day_of_quarter,
+        (DATE_TRUNC('quarter', datum) + INTERVAL '3 MONTH - 1 day')::DATE AS last_day_of_quarter,
+        TO_DATE(EXTRACT(YEAR FROM datum) || '-01-01', 'YYYY-MM-DD') AS first_day_of_year,
+        TO_DATE(EXTRACT(YEAR FROM datum) || '-12-31', 'YYYY-MM-DD') AS last_day_of_year,
+        TO_CHAR(datum, 'mmyyyy') AS mmyyyy,
+        TO_CHAR(datum, 'mmddyyyy') AS mmddyyyy,
+        CASE
+            WHEN EXTRACT(ISODOW FROM datum) IN (6, 7) THEN TRUE
+            ELSE FALSE
+            END AS weekend_indr
+    FROM (SELECT sdate + SEQUENCE.DAY AS datum
+        FROM GENERATE_SERIES(0, nm - 1) AS SEQUENCE (DAY)
+        GROUP BY SEQUENCE.DAY) DQ
+    ORDER BY 1;
 
     -- Load core.fact_payment
 
@@ -201,7 +250,7 @@ begin
     RAISE NOTICE '================================================';
 	
     CALL core.truncate_fact_tables();
-    CALL core.load_core();
+    CALL core.load_core('2007-01-01'::DATE, 5844);
 
     RAISE NOTICE '==========================================';
     RAISE NOTICE 'FULL CORE LAYER LOADING is Completed';
