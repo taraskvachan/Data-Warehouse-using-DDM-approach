ed5f83dda34e11b1d07e023a918321488067ca5f
taraskvachan <107889485+taraskvachan@users.noreply.github.com>
2025-03-14 19:43:26 +0300

    Update proc_sales_by_dates.sql

--- report/proc_sales_by_dates.sql
+++ report/proc_sales_by_dates.sql
@@ -9,35 +9,34 @@ Script Purpose:
 ===============================================================================
 */
 
-
 CREATE OR REPLACE PROCEDURE report.sales_by_dates_calc()
 LANGUAGE plpgsql
 AS $$
 BEGIN
-		RAISE NOTICE '>> Truncating Table: report.sales_by_dates';
-		TRUNCATE TABLE report.sales_by_dates;
+	RAISE NOTICE '>> Truncating Table: report.sales_by_dates';
+	TRUNCATE TABLE report.sales_by_dates;
 
-		RAISE NOTICE '>> Inserting Data Into: report.sales_by_dates';	
-		INSERT INTO report.sales_by_dates (
+	RAISE NOTICE '>> Inserting Data Into: report.sales_by_dates';	
+	INSERT INTO report.sales_by_dates (
 		date_title,
 		amount,
 		date_sort
-		)
-		select 
-			dd.day_of_month || ' ' || dd.month_name || ' ' || dd.year_actual AS date_title,
-			sum(fp.amount) as amount,
-			dd.date_dim_pk as date_sort
-		from 
-			core.fact_payment fp
-		join core.dim_date dd
-			on fp.payment_date_fk = dd.date_dim_pk
-		group by
-			dd.day_of_month || ' ' || dd.month_name || ' ' || dd.year_actual,
-			dd.date_dim_pk; 
+	)
+	SELECT 
+		dd.day_of_month || ' ' || dd.month_name || ' ' || dd.year_actual AS date_title,
+		sum(fp.amount) as amount,
+		dd.date_dim_pk as date_sort
+	FROM 
+		core.fact_payment fp
+	JOIN core.dim_date dd
+		ON fp.payment_date_fk = dd.date_dim_pk
+	GROUP BY
+		dd.day_of_month || ' ' || dd.month_name || ' ' || dd.year_actual,
+		dd.date_dim_pk; 
 END;
 $$;
 
-call report.sales_by_dates_calc()
+CALL report.sales_by_dates_calc()
 
 
 	
